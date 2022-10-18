da9d4efaf524aadf999a871154114ba5c642f5d0
straxFromIbr <haga.yuya2.2016@gmail.com>
2022-10-18 22:56:34 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+
+from tweet_sender import twitter
+from tweet_sender.senders import (
+    DiscordSender,
+    FileSender,
+    RemoteFileSender,
+    StdOutSender,
+)
+
+
+def main():
+    with open("./cred.json") as f:
+        api_keys = json.load(f)
+    target = api_keys["target"]
+    host = api_keys["host"]
+    host_path = api_keys["host_path"]
+
+    senders = [
+        FileSender(Path("./tweets.txt")),
+        StdOutSender(),
+        DiscordSender(api_keys["discord_webhook_url"]),
+        RemoteFileSender(host=host, host_path=host_path),
+    ]
+
+    tweet_fether = twitter.TweetFetcher(
+        api_keys=api_keys,
+        target=target,
+        senders=senders,
+    )
+    tweet_fether.send()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tweet_sender/twitter.py
@@ -0,0 +1,90 @@
+import json
+import os
+from typing import List
+
+from requests_oauthlib import OAuth1Session
+
+from .senders import Sender
+
+
+class TweetFetcher:
+    api_root = "https://api.twitter.com/2"
+
+    def __init__(
+        self,
+        api_keys: dict,
+        target,
+        senders: List[Sender],
+        max_id=None,
+        prefix="",
+        since_id_path="since_id",
+    ):
+        # APIキーの設定
+        cons_key = api_keys["cons_key"]
+        cons_sec = api_keys["cons_sec"]
+        access_token = api_keys["access_token"]
+        access_sec = api_keys["access_sec"]
+        self.session = OAuth1Session(cons_key, cons_sec, access_token, access_sec)
+        self.api_url = f"{self.api_root}/users/{target}/tweets"
+
+        self.prefix = prefix
+
+        if max_id is not None:
+            self.since_id = max_id
+
+        self.senders = senders
+        self.since_id_path = since_id_path
+
+    def get_tweet(self, max_results=None):
+        params = {}
+        if max_results is not None:
+            params["max_results"] = max_results
+
+        # 前回のツイート取得じに保存したmax_idを取得
+        if self.since_id is not None:
+            params["since_id"] = self.since_id
+
+        result = self.session.get(self.api_url, params=params)
+        result_json = result.json()
+        if result_json["meta"]["result_count"] == 0:
+            return None
+
+        self.since_id = result_json["meta"]["newest_id"]
+        latest_tweets = list(map(lambda t: t["text"], result_json["data"]))
+        return latest_tweets
+
+    def send(self):
+        tweets = self.get_tweet()
+        if tweets is None:
+            print("tweets is None")
+            return
+
+        tweet = self.prefix + tweets[0]
+        for sender in self.senders:
+            sender.send(tweet)
+
+    @property
+    def since_id(self):
+        since_id = self._load()
+        if since_id is None:
+            return "0"
+        return since_id["since_id"]
+
+    @since_id.setter
+    def since_id(self, since_id):
+        if since_id is None:
+            since_id = "0"
+        self._dump({"since_id": since_id})
+
+    def _dump(self, obj: dict):
+        with open(self.since_id_path, "w") as f:
+            json.dump(obj, f)
+
+    def _load(self):
+        fname = self.since_id_path
+        if not os.path.exists(fname):
+            return None
+
+        with open(fname, "r") as f:
+            result = json.load(f)
+        return result
